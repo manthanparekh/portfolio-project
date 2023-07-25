2ebe138bda93267bd110e4c557a058b5280c6df1
manthanparekh <manthan.parekh@outlook.com>
2023-07-24 21:57:17 -0400

    data.js code

--- .history/src/Components/Projects_20230723200630.js
+++ .history/src/Components/Projects_20230723200630.js
@@ -23,9 +23,9 @@ export default function Projects() {
                 className="sm:w-1/2 w-100 p-4">
                 <div className="flex relative">
                     <img
-                        alt="gallery"
-                        className="absolute inset-0 w-full h-full object-cover object-center"
-                        src={project.image}
+                    alt="gallery"
+                    className="absolute inset-0 w-full h-full object-cover object-center"
+                    src={project.image}
                     />
                     <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
---
+++ src/data.js
@@ -0,0 +1,35 @@
+
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:
+                // TODO add description
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:
+                // TODO add description
+        image: "./project-2.gif",
+        link: "https://reedbarger.com",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+                // TODO add description
+        image: "./project-3.gif",
+        link: "https://jsbootcamp.com",
+    },
+    {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+            // TODO add description
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+    },
+];
